60094f068e2476a70c38bad0260f9ff19537580b
uvjoshi <uvjoshi2005@gmail.com>
2023-06-22 17:45:58 +0530

    k

---
+++ arr.js
@@ -0,0 +1,7 @@
+const arr = ["Yuvraj" , 23 , ["Football" , "Hockey"],69];
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+// console.log(arr[4]);
---
+++ msg.js
@@ -0,0 +1,6 @@
+
+function msg () 
+{
+ alert("This a Script")  ;  
+}
+
